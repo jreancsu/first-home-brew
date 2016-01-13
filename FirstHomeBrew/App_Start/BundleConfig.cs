7e3f519b22af6a609afabbd7d1dd1118b187fd98
Jason Rea <jrea@ebsco.com>
2016-01-13 00:41:53 -0500

    upgraded jquery, added bootstrap, added content

--- FirstHomeBrew/App_Start/BundleConfig.cs
+++ FirstHomeBrew/App_Start/BundleConfig.cs
@@ -8,27 +8,33 @@ namespace FirstHomeBrew
 		// For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
 		public static void RegisterBundles(BundleCollection bundles)
 		{
-			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-{version}.js"));
+			bundles.Add(new ScriptBundle("~/bundles/jquery")
+				.Include("~/Scripts/jquery-{version}.js"));
 
-			bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-						"~/Scripts/jquery-ui-{version}.js"));
+			bundles.Add(new ScriptBundle("~/bundles/jqueryui")
+				.Include("~/Scripts/jquery-ui-{version}.js"));
 
-			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-						"~/Scripts/jquery.unobtrusive*",
+			bundles.Add(new ScriptBundle("~/bundles/bootstrap")
+				.Include("~/Scripts/bootstrap.js"));
+
+			bundles.Add(new ScriptBundle("~/bundles/jqueryval")
+				.Include("~/Scripts/jquery.unobtrusive*",
 						"~/Scripts/jquery.validate*"));
 			
-			bundles.Add(new ScriptBundle("~/bundles/navigation").Include(
-						"~/Scripts/Navigation/jquery.slicknav.js"));
+			bundles.Add(new ScriptBundle("~/bundles/navigation")
+				.Include("~/Scripts/Navigation/jquery.slicknav.js"));
 
 			// Use the development version of Modernizr to develop with and learn from. Then, when you're
 			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-						"~/Scripts/modernizr-*"));
+			bundles.Add(new ScriptBundle("~/bundles/modernizr")
+				.Include("~/Scripts/modernizr-*"));
 
-			bundles.Add(new StyleBundle("~/Content/css").Include(
-				"~/Content/Site.css",
-				"~/Content/slicknav.css"));
+			bundles.Add(new StyleBundle("~/Content/css")
+				.Include("~/Content/slicknav.css",
+				"~/Content/Site.css"));
+
+			bundles.Add(new StyleBundle("~/Content/bootstrap")
+				.Include("~/Content/bootstrap/bootstrap.css"));
 
 			bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
 						"~/Content/themes/base/jquery.ui.core.css",
