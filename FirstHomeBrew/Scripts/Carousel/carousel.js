5adc8f5d87d7e18d61d1117a7f567be913e1121d
Jason Rea <jrea@ebsco.com>
2016-03-19 12:15:13 -0400

    got that carousel working finally.....good grief

--- FirstHomeBrew/Scripts/Carousel/carousel.js
+++ FirstHomeBrew/Scripts/Carousel/carousel.js
@@ -1,3 +1,29 @@
-﻿$(function() {
-    $('.slick-carousel').slick();
-});
+﻿//This is freaking dumb that I have to do it this way.  For some reason the adaptive height
+//with slick carousel doesn't work if you call .slick() after DOM ready and the images haven't
+//finished downloading.
+window.onload = function () {
+    $('.slick-carousel').slick({
+        infinite: true,
+        lazyLoad: 'progressive', //Presumably this allows window.onload to fire after the first
+                                 //image finishes downloading.
+        speed: 500,
+        variableWidth: false,
+        responsive: [
+        {
+            breakpoint: 2048,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }]
+    });
+
+    $('.slick-carousel img').css('display', 'block');
+};
