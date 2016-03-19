b0a0070f28c652e2181e2392607ad5453d91d68d
Jason Rea <jrea@ebsco.com>
2016-03-19 12:54:28 -0400

    adjusted responsive breakpoints for carousel

--- FirstHomeBrew/Scripts/Carousel/carousel.js
+++ FirstHomeBrew/Scripts/Carousel/carousel.js
@@ -17,10 +17,17 @@ window.onload = function () {
             }
         },
         {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        },
+        {
             breakpoint: 768,
             settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToShow: 1,
+                slidesToScroll: 1
             }
         }]
     });
